92567d9b500f5c0d086471edbe2d4ce4e0e35b73
drawjk705 <33524614+drawjk705@users.noreply.github.com>
2017-12-05 15:00:23 -0500

    Add files via upload

---
+++ BookObject/src/Secret.java
@@ -0,0 +1,11 @@
+
+
+public class Secret {
+
+	public static final String username = "8fbb2096-7263-40ae-81b6-c57bce1c63d2";
+	public static final String password = "gARQHJuRUtye";
+		
+	public Secret() {
+	}
+	
+}
