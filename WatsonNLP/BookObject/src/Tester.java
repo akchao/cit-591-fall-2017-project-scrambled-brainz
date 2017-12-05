3d9a30dfb4cfbad06e58e9cfe362b77d8e093d81
drawjk705 <33524614+drawjk705@users.noreply.github.com>
2017-12-05 14:57:38 -0500

    Add files via upload

---
+++ WatsonNLP/BookObject/src/Tester.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+/**
+ * tester class for book functions
+ *
+ * @author Drawjk705
+ *
+ */
+
+public class Tester {
+
+	public static void main(String[] args) throws IOException {
+
+		BookReader br = new BookReader();
+		
+//		br.URLReader();
+	}
+}
