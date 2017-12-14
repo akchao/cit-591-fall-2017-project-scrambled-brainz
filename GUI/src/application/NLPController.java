37712607bd9ca02f3c9b64a487bf59acd4c972e4
drawjk705 <drawjk705@gmail.com>
2017-12-14 15:37:16 -0500

    added comments to GUI code

--- GUI/src/application/NLPController.java
+++ GUI/src/application/NLPController.java
@@ -1,52 +1,44 @@
 package application;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-
-import org.omg.CORBA.INITIALIZE;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 public class NLPController implements Initializable {
 	
+	// mapping between book title to the book object itself
 	private HashMap<String, BookDummy> titleToBook = new HashMap<String, BookDummy>();	
 
+	// list of books to display
 	private ObservableList<String> list;
 	
+	// "switch" to determine if should plot book comparisons or not
 	private Boolean booksCompared;
 
-// graph series
-		
+	// dynamic text for the application
 	@FXML
     private Text currentBook;
 	@FXML
 	private Text comparedBook;
 	
-	
+	// lists to display
     @FXML
     private ListView<String> bookList;
     @FXML
     private ListView<String> comparisonList;
+    
+    // buttons
     @FXML
     private Button selectBook;
     @FXML
@@ -54,9 +46,11 @@ public class NLPController implements Initializable {
     @FXML
     private Button updateGraph;
     @FXML
+    
+    // the line chart
     private LineChart<String, Double> lineChart;
 
-// checkboxes    
+    // checkboxes    
     @FXML
     private CheckBox sadCheck;
     @FXML
@@ -68,15 +62,20 @@ public class NLPController implements Initializable {
     @FXML
     private CheckBox angerCheck;
     
-    
+    /**
+     * method to set up initial features
+     */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		// set to false
 		booksCompared = false;
 		
 		// set currentBook text
 		currentBook.setText("No book chosen. Yet...");
+		// set comparedBook text
+		comparedBook.setText("None");
 		
+		// dummy books to load into the bookList
 		BookDummy b1 = new BookDummy("Help!");
 		BookDummy b2 = new BookDummy("Abbey Road");	
 		
@@ -87,20 +86,25 @@ public class NLPController implements Initializable {
 		list = FXCollections.observableArrayList(b1.getTitle(), b2.getTitle());
 		bookList.setItems(list);
 		
-		// have checkboxes preset to be checked
+		// have check-boxes preset to be checked
 		sadCheck.fire();
 		joyCheck.fire();
 		fearCheck.fire();
 		disgustCheck.fire();
 		angerCheck.fire();
-		
 	}
     
-    
+    /**
+     * method to generate the graph
+     * @param event when a button has been clicked
+     */
     @FXML
     void generateGraph(ActionEvent event) {
+    	// clear the graph
     	lineChart.getData().clear();
     	
+    	// make a second list for books to compare
+    	// will display all books except for the one that is currently selected
     	ObservableList<String> list2 = FXCollections.observableArrayList((list));
     	
     	String bd = null;
@@ -108,12 +112,16 @@ public class NLPController implements Initializable {
     	// if are selecting a book without comparing
     	if (event.getSource().equals(selectBook) || event.getSource().equals(updateGraph) ||
     			event.getSource().equals(compareWith)) {
+    		// get the selected book
     		bd = bookList.getSelectionModel().getSelectedItem();
+    		// set the currentBook text
     		currentBook.setText(bd);
+    		// remove this book from the comparison list
     		list2.remove(bd);
+    		// set the comparison list
         	comparisonList.setItems(list2);
         	
-        	
+        	// book dummy object
     	    BookDummy book = titleToBook.get(bd);
 
     	    // declare series
@@ -123,7 +131,7 @@ public class NLPController implements Initializable {
     		XYChart.Series<String, Double> disgust = new XYChart.Series<>();
     		XYChart.Series<String, Double> joy = new XYChart.Series<>();
 
-    		
+    		// plot series data
     		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
     		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
     		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
@@ -167,10 +175,14 @@ public class NLPController implements Initializable {
     	// if are comparing two books
     	if (event.getSource().equals(compareWith) || (event.getSource().equals(updateGraph) &&
     			booksCompared)) {
+    		// get the selected book
     		bd = comparisonList.getSelectionModel().getSelectedItem();
+    		// set comparedBook text
     		comparedBook.setText(bd);
-        	booksCompared = true;
+        	// set "switch" to true
+    		booksCompared = true;
         	
+    		// new book dummy
     	    BookDummy book = titleToBook.get(bd);
 
     	    // declare series
@@ -180,7 +192,7 @@ public class NLPController implements Initializable {
     		XYChart.Series<String, Double> disgust = new XYChart.Series<>();
     		XYChart.Series<String, Double> joy = new XYChart.Series<>();
 
-    		
+    		// plot series
     		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
     		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
     		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
@@ -221,154 +233,18 @@ public class NLPController implements Initializable {
     			lineChart.getData().add(fear);
     		}
     	}
-
-		
-//	    BookDummy book = titleToBook.get(bd);
-//
-//	    // declare series
-//	    XYChart.Series<String, Double> anger = new XYChart.Series<>();
-//		XYChart.Series<String, Double> fear = new XYChart.Series<>();
-//		XYChart.Series<String, Double> sadness = new XYChart.Series<>();
-//		XYChart.Series<String, Double> disgust = new XYChart.Series<>();
-//		XYChart.Series<String, Double> joy = new XYChart.Series<>();
-//
-//		
-//		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
-//		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
-//		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
-//		anger.setName("Anger");
-//		if (angerCheck.isSelected()) {
-//			lineChart.getData().add(anger);
-//		}
-//		
-//		joy.getData().add(new XYChart.Data<String, Double>("1", book.getJoy(0)));
-//		joy.getData().add(new XYChart.Data<String, Double>("2", book.getJoy(1)));
-//		joy.getData().add(new XYChart.Data<String, Double>("3", book.getJoy(2)));
-//		joy.setName("Joy");
-//		if (joyCheck.isSelected()) {
-//			lineChart.getData().add(joy);
-//		}		
-//		
-//		disgust.getData().add(new XYChart.Data<String, Double>("1", book.getDisgust(0)));
-//		disgust.getData().add(new XYChart.Data<String, Double>("2", book.getDisgust(1)));
-//		disgust.getData().add(new XYChart.Data<String, Double>("3", book.getDisgust(2)));
-//		disgust.setName("Disgust");
-//		if (disgustCheck.isSelected()) {
-//			lineChart.getData().add(disgust);
-//		}
-//		
-//		sadness.getData().add(new XYChart.Data<String, Double>("1", book.getSadness(0)));
-//		sadness.getData().add(new XYChart.Data<String, Double>("2", book.getSadness(1)));
-//		sadness.getData().add(new XYChart.Data<String, Double>("3", book.getSadness(2)));
-//		sadness.setName("Sadness");
-//		if (sadCheck.isSelected()) {
-//			lineChart.getData().add(sadness);
-//		}
-//		
-//		fear.getData().add(new XYChart.Data<String, Double>("1", book.getFear(0)));
-//		fear.getData().add(new XYChart.Data<String, Double>("2", book.getFear(1)));
-//		fear.getData().add(new XYChart.Data<String, Double>("3", book.getFear(2)));
-//		fear.setName("Fear");
-//		if (fearCheck.isSelected()) {
-//			lineChart.getData().add(fear);
-//		}
     }
-       
+     
+    /**
+     * method for Undo Comparison button
+     * @param event when the undo comparison button is clicked
+     */
     @FXML
     public void undoCompare(ActionEvent event) {
+    	// turns "switch" off
     	booksCompared = false;
     	
+    	// resets comparedBook text
     	comparedBook.setText("None");
     }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	@FXML
-//	private Button selectBook;
-//	// to list the title of the current book
-//	@FXML
-//	private Text currentBook;
-//	@FXML
-//	private Button compareWith;
-//	
-//	@FXML LineChart<String, Double> lineChart;
-//	
-//	private BookDummy book;
-//	
-//	
-//
-//	
-//	/**
-//	 * method to display list of books from which
-//	 * user can select to analyze 
-//	 * @param event
-//	 * @throws IOException 
-//	 */
-//	@FXML
-//	public void displayBookList(ActionEvent event) throws IOException {
-//		Stage stage;
-//		Parent root;
-//		stage = new Stage();
-//		root = FXMLLoader.load(getClass().getResource("BookSelect.fxml"));
-//		stage.setScene(new Scene(root));
-//		stage.initModality(Modality.APPLICATION_MODAL);
-//		stage.initOwner(selectBook.getScene().getWindow());
-//		stage.showAndWait();
-//	}
-//	
-//	@FXML
-//	public void populateGraph(ActionEvent event) {
-//		BookSelectController bsc = new BookSelectController();
-//		
-//		BookDummy book = bsc.returnSelectedBook();
-//		
-//		
-//		
-////		lineChart.getData().clear();
-//		
-//		System.out.println(book.getAnger(1));
-//		
-////		BookDummy book = bsc.returnSelectedBook();
-//		
-//		XYChart.Series<String, Double> anger = new XYChart.Series<>();
-//		
-//		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
-//		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
-//		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
-//		anger.setName("Anger");
-////		lineChart.getData().add(anger);
-//		
-//		XYChart.Series<String, Double> joy = new XYChart.Series<>();
-//		
-//		joy.getData().add(new XYChart.Data<String, Double>("1", book.getJoy(0)));
-//		joy.getData().add(new XYChart.Data<String, Double>("2", book.getJoy(1)));
-//		joy.getData().add(new XYChart.Data<String, Double>("3", book.getJoy(2)));
-//		joy.setName("Joy");
-//		
-////		lineChart.getData().add(joy);
-//		
-//		
-//		
-//	}
-//	
-//	public void setBook(BookDummy book) {
-//		this.book = book;
-//	}
 }
