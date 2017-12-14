14eee0a8d6f64f09106de0a7ad506412e70bd197
drawjk705 <drawjk705@gmail.com>
2017-12-14 15:17:41 -0500

    completed GUI comparison functionality

--- GUI/src/application/NLPController.java
+++ GUI/src/application/NLPController.java
@@ -32,19 +32,31 @@ public class NLPController implements Initializable {
 	private HashMap<String, BookDummy> titleToBook = new HashMap<String, BookDummy>();	
 
 	private ObservableList<String> list;
+	
+	private Boolean booksCompared;
 
+// graph series
+		
 	@FXML
     private Text currentBook;
+	@FXML
+	private Text comparedBook;
+	
+	
     @FXML
     private ListView<String> bookList;
+    @FXML
+    private ListView<String> comparisonList;
     @FXML
     private Button selectBook;
     @FXML
     private Button compareWith;
     @FXML
+    private Button updateGraph;
+    @FXML
     private LineChart<String, Double> lineChart;
 
-    
+// checkboxes    
     @FXML
     private CheckBox sadCheck;
     @FXML
@@ -59,7 +71,10 @@ public class NLPController implements Initializable {
     
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-//		set currentBook text
+		
+		booksCompared = false;
+		
+		// set currentBook text
 		currentBook.setText("No book chosen. Yet...");
 		
 		BookDummy b1 = new BookDummy("Help!");
@@ -68,64 +83,203 @@ public class NLPController implements Initializable {
 		titleToBook.put(b1.getTitle(), b1);
 		titleToBook.put(b2.getTitle(), b2);
 		
+		// have all books loaded into the list
 		list = FXCollections.observableArrayList(b1.getTitle(), b2.getTitle());
-	
 		bookList.setItems(list);
+		
+		// have checkboxes preset to be checked
+		sadCheck.fire();
+		joyCheck.fire();
+		fearCheck.fire();
+		disgustCheck.fire();
+		angerCheck.fire();
+		
 	}
     
     
     @FXML
     void generateGraph(ActionEvent event) {
-    	String bd;
-		bd = bookList.getSelectionModel().getSelectedItem();
-		currentBook.setText(bd);
-		
-	    BookDummy book = titleToBook.get(bd);
-
-    	
     	lineChart.getData().clear();
-		
-		XYChart.Series<String, Double> anger = new XYChart.Series<>();
-		
-		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
-		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
-		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
-		anger.setName("Anger");
-		lineChart.getData().add(anger);
-		
-		XYChart.Series<String, Double> joy = new XYChart.Series<>();
-		
-		joy.getData().add(new XYChart.Data<String, Double>("1", book.getJoy(0)));
-		joy.getData().add(new XYChart.Data<String, Double>("2", book.getJoy(1)));
-		joy.getData().add(new XYChart.Data<String, Double>("3", book.getJoy(2)));
-		joy.setName("Joy");
-		lineChart.getData().add(joy);
-		
-		XYChart.Series<String, Double> disgust = new XYChart.Series<>();
-		
-		disgust.getData().add(new XYChart.Data<String, Double>("1", book.getDisgust(0)));
-		disgust.getData().add(new XYChart.Data<String, Double>("2", book.getDisgust(1)));
-		disgust.getData().add(new XYChart.Data<String, Double>("3", book.getDisgust(2)));
-		disgust.setName("Disgust");
-		lineChart.getData().add(disgust);
-		
-		XYChart.Series<String, Double> sadness = new XYChart.Series<>();
-		
-		sadness.getData().add(new XYChart.Data<String, Double>("1", book.getSadness(0)));
-		sadness.getData().add(new XYChart.Data<String, Double>("2", book.getSadness(1)));
-		sadness.getData().add(new XYChart.Data<String, Double>("3", book.getSadness(2)));
-		sadness.setName("Sadness");
-		lineChart.getData().add(sadness);
-		
-		XYChart.Series<String, Double> fear = new XYChart.Series<>();
-		
-		fear.getData().add(new XYChart.Data<String, Double>("1", book.getFear(0)));
-		fear.getData().add(new XYChart.Data<String, Double>("2", book.getFear(1)));
-		fear.getData().add(new XYChart.Data<String, Double>("3", book.getFear(2)));
-		fear.setName("Joy");
-		lineChart.getData().add(fear);
-    }
+    	
+    	ObservableList<String> list2 = FXCollections.observableArrayList((list));
+    	
+    	String bd = null;
+    	
+    	// if are selecting a book without comparing
+    	if (event.getSource().equals(selectBook) || event.getSource().equals(updateGraph) ||
+    			event.getSource().equals(compareWith)) {
+    		bd = bookList.getSelectionModel().getSelectedItem();
+    		currentBook.setText(bd);
+    		list2.remove(bd);
+        	comparisonList.setItems(list2);
+        	
+        	
+    	    BookDummy book = titleToBook.get(bd);
 
+    	    // declare series
+    	    XYChart.Series<String, Double> anger = new XYChart.Series<>();
+    		XYChart.Series<String, Double> fear = new XYChart.Series<>();
+    		XYChart.Series<String, Double> sadness = new XYChart.Series<>();
+    		XYChart.Series<String, Double> disgust = new XYChart.Series<>();
+    		XYChart.Series<String, Double> joy = new XYChart.Series<>();
+
+    		
+    		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
+    		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
+    		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
+    		anger.setName(bd + ": Anger");
+    		if (angerCheck.isSelected()) {
+    			lineChart.getData().add(anger);
+    		}
+    		
+    		joy.getData().add(new XYChart.Data<String, Double>("1", book.getJoy(0)));
+    		joy.getData().add(new XYChart.Data<String, Double>("2", book.getJoy(1)));
+    		joy.getData().add(new XYChart.Data<String, Double>("3", book.getJoy(2)));
+    		joy.setName(bd + ": Joy");
+    		if (joyCheck.isSelected()) {
+    			lineChart.getData().add(joy);
+    		}		
+    		
+    		disgust.getData().add(new XYChart.Data<String, Double>("1", book.getDisgust(0)));
+    		disgust.getData().add(new XYChart.Data<String, Double>("2", book.getDisgust(1)));
+    		disgust.getData().add(new XYChart.Data<String, Double>("3", book.getDisgust(2)));
+    		disgust.setName(bd + ": Disgust");
+    		if (disgustCheck.isSelected()) {
+    			lineChart.getData().add(disgust);
+    		}
+    		
+    		sadness.getData().add(new XYChart.Data<String, Double>("1", book.getSadness(0)));
+    		sadness.getData().add(new XYChart.Data<String, Double>("2", book.getSadness(1)));
+    		sadness.getData().add(new XYChart.Data<String, Double>("3", book.getSadness(2)));
+    		sadness.setName(bd + ": Sadness");
+    		if (sadCheck.isSelected()) {
+    			lineChart.getData().add(sadness);
+    		}
+    		
+    		fear.getData().add(new XYChart.Data<String, Double>("1", book.getFear(0)));
+    		fear.getData().add(new XYChart.Data<String, Double>("2", book.getFear(1)));
+    		fear.getData().add(new XYChart.Data<String, Double>("3", book.getFear(2)));
+    		fear.setName(bd + ": Fear");
+    		if (fearCheck.isSelected()) {
+    			lineChart.getData().add(fear);
+    		}
+    	}
+    	// if are comparing two books
+    	if (event.getSource().equals(compareWith) || (event.getSource().equals(updateGraph) &&
+    			booksCompared)) {
+    		bd = comparisonList.getSelectionModel().getSelectedItem();
+    		comparedBook.setText(bd);
+        	booksCompared = true;
+        	
+    	    BookDummy book = titleToBook.get(bd);
+
+    	    // declare series
+    	    XYChart.Series<String, Double> anger = new XYChart.Series<>();
+    		XYChart.Series<String, Double> fear = new XYChart.Series<>();
+    		XYChart.Series<String, Double> sadness = new XYChart.Series<>();
+    		XYChart.Series<String, Double> disgust = new XYChart.Series<>();
+    		XYChart.Series<String, Double> joy = new XYChart.Series<>();
+
+    		
+    		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
+    		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
+    		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
+    		anger.setName(bd + ": Anger");
+    		if (angerCheck.isSelected()) {
+    			lineChart.getData().add(anger);
+    		}
+    		
+    		joy.getData().add(new XYChart.Data<String, Double>("1", book.getJoy(0)));
+    		joy.getData().add(new XYChart.Data<String, Double>("2", book.getJoy(1)));
+    		joy.getData().add(new XYChart.Data<String, Double>("3", book.getJoy(2)));
+    		joy.setName(bd + ": Joy");
+    		if (joyCheck.isSelected()) {
+    			lineChart.getData().add(joy);
+    		}		
+    		
+    		disgust.getData().add(new XYChart.Data<String, Double>("1", book.getDisgust(0)));
+    		disgust.getData().add(new XYChart.Data<String, Double>("2", book.getDisgust(1)));
+    		disgust.getData().add(new XYChart.Data<String, Double>("3", book.getDisgust(2)));
+    		disgust.setName(bd + ": Disgust");
+    		if (disgustCheck.isSelected()) {
+    			lineChart.getData().add(disgust);
+    		}
+    		
+    		sadness.getData().add(new XYChart.Data<String, Double>("1", book.getSadness(0)));
+    		sadness.getData().add(new XYChart.Data<String, Double>("2", book.getSadness(1)));
+    		sadness.getData().add(new XYChart.Data<String, Double>("3", book.getSadness(2)));
+    		sadness.setName(bd + ": Sadness");
+    		if (sadCheck.isSelected()) {
+    			lineChart.getData().add(sadness);
+    		}
+    		
+    		fear.getData().add(new XYChart.Data<String, Double>("1", book.getFear(0)));
+    		fear.getData().add(new XYChart.Data<String, Double>("2", book.getFear(1)));
+    		fear.getData().add(new XYChart.Data<String, Double>("3", book.getFear(2)));
+    		fear.setName(bd + ": Fear");
+    		if (fearCheck.isSelected()) {
+    			lineChart.getData().add(fear);
+    		}
+    	}
+
+		
+//	    BookDummy book = titleToBook.get(bd);
+//
+//	    // declare series
+//	    XYChart.Series<String, Double> anger = new XYChart.Series<>();
+//		XYChart.Series<String, Double> fear = new XYChart.Series<>();
+//		XYChart.Series<String, Double> sadness = new XYChart.Series<>();
+//		XYChart.Series<String, Double> disgust = new XYChart.Series<>();
+//		XYChart.Series<String, Double> joy = new XYChart.Series<>();
+//
+//		
+//		anger.getData().add(new XYChart.Data<String, Double>("1", book.getAnger(0)));
+//		anger.getData().add(new XYChart.Data<String, Double>("2", book.getAnger(1)));
+//		anger.getData().add(new XYChart.Data<String, Double>("3", book.getAnger(2)));
+//		anger.setName("Anger");
+//		if (angerCheck.isSelected()) {
+//			lineChart.getData().add(anger);
+//		}
+//		
+//		joy.getData().add(new XYChart.Data<String, Double>("1", book.getJoy(0)));
+//		joy.getData().add(new XYChart.Data<String, Double>("2", book.getJoy(1)));
+//		joy.getData().add(new XYChart.Data<String, Double>("3", book.getJoy(2)));
+//		joy.setName("Joy");
+//		if (joyCheck.isSelected()) {
+//			lineChart.getData().add(joy);
+//		}		
+//		
+//		disgust.getData().add(new XYChart.Data<String, Double>("1", book.getDisgust(0)));
+//		disgust.getData().add(new XYChart.Data<String, Double>("2", book.getDisgust(1)));
+//		disgust.getData().add(new XYChart.Data<String, Double>("3", book.getDisgust(2)));
+//		disgust.setName("Disgust");
+//		if (disgustCheck.isSelected()) {
+//			lineChart.getData().add(disgust);
+//		}
+//		
+//		sadness.getData().add(new XYChart.Data<String, Double>("1", book.getSadness(0)));
+//		sadness.getData().add(new XYChart.Data<String, Double>("2", book.getSadness(1)));
+//		sadness.getData().add(new XYChart.Data<String, Double>("3", book.getSadness(2)));
+//		sadness.setName("Sadness");
+//		if (sadCheck.isSelected()) {
+//			lineChart.getData().add(sadness);
+//		}
+//		
+//		fear.getData().add(new XYChart.Data<String, Double>("1", book.getFear(0)));
+//		fear.getData().add(new XYChart.Data<String, Double>("2", book.getFear(1)));
+//		fear.getData().add(new XYChart.Data<String, Double>("3", book.getFear(2)));
+//		fear.setName("Fear");
+//		if (fearCheck.isSelected()) {
+//			lineChart.getData().add(fear);
+//		}
+    }
+       
+    @FXML
+    public void undoCompare(ActionEvent event) {
+    	booksCompared = false;
+    	
+    	comparedBook.setText("None");
+    }
 	
 	
 	
