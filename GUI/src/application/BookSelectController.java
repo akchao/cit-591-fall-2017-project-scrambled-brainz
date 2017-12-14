80d8486588d0e84b92933b263cd5361ddd16590b
drawjk705 <drawjk705@gmail.com>
2017-12-14 01:07:11 -0500

    got pop-up to return results to main window

--- GUI/src/application/BookSelectController.java
+++ GUI/src/application/BookSelectController.java
@@ -1,33 +1,54 @@
 package application;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class BookSelectController implements Initializable {
 	
-	private ObservableList<BookDummy> list;
+	private ObservableList<String> list;
+	
+	private HashMap<String, BookDummy> titleToBook = new HashMap<String, BookDummy>();	
 	
 	@FXML
 	private Button selectBook2;
 	// list view of all books
 	@FXML
-	private ListView<BookDummy> bookList;
+	private ListView<String> bookList;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		BookDummy b1 = new BookDummy("Help");
+		BookDummy b1 = new BookDummy("Help!");
 		BookDummy b2 = new BookDummy("Abbey Road");	
 		
-		list = FXCollections.observableArrayList(b1, b2);
+		titleToBook.put(b1.getTitle(), b1);
+		titleToBook.put(b2.getTitle(), b2);
+		
+		list = FXCollections.observableArrayList(b1.getTitle(), b2.getTitle());
 	
 		bookList.setItems(list);
 	}
+	
+	
+	@FXML
+	public BookDummy selectBook(ActionEvent event) {
+		String bd;
+		bd = bookList.getSelectionModel().getSelectedItem();
+		
+		Stage stage = (Stage) selectBook2.getScene().getWindow();
+	    stage.close();
+
+		return titleToBook.get(bd);
+	}
+	
 	
 }
