32177c4a1ea176184415ab41877ec877f73a4b2a
Alice Chao <Alice@Alices-MBP.fios-router.home>
2017-12-17 15:21:42 -0500

    added additional Junit tests to WatsonParserTest

--- GUI/Test/application/WatsonParserTest.java
+++ GUI/Test/application/WatsonParserTest.java
@@ -1,6 +1,7 @@
 package application;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,8 +9,10 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gson.JsonParseException;
+
 /**
- * Test methods from WatsonParserTest
+ * Test methods for WatsonParser 
  * @author Alice
  *
  */
@@ -19,11 +22,23 @@ public class WatsonParserTest {
 	String sampleEntityPerson;
 	String sampleEntityNonPerson;
 	String sampleDocumentEmotion;
-	List<String> sample;
+	List<String> sampleEntityList;
 	
 	@Before
-	public void setupWatsonTest() {
+	public void setupWatsonParserTest() {
 		wp = new WatsonParser();
+		
+		sampleDocumentEmotion = "{\n" + 
+				"  \"document\": {\n" + 
+				"    \"emotion\": {\n" + 
+				"      \"anger\": 0.144436,\n" + 
+				"      \"disgust\": 0.074289,\n" + 
+				"      \"fear\": 0.130087,\n" + 
+				"      \"joy\": 0.599415,\n" + 
+				"      \"sadness\": 0.519859\n" + 
+				"    }\n" + 
+				"  }\n" + 
+				"}";
 		
 		sampleEntityPerson = "{\n" + 
 				"  \"type\": \"Person\",\n" + 
@@ -57,65 +72,73 @@ public class WatsonParserTest {
 				"  \"sentiment\": {\n" + 
 				"    \"score\": 0.0\n" + 
 				"  }\n" + 
-				"}";
+				"}";	
 		
-		sampleDocumentEmotion = "{\n" + 
-				"  \"document\": {\n" + 
-				"    \"emotion\": {\n" + 
-				"      \"anger\": 0.144436,\n" + 
-				"      \"disgust\": 0.074289,\n" + 
-				"      \"fear\": 0.130087,\n" + 
-				"      \"joy\": 0.599415,\n" + 
-				"      \"sadness\": 0.519859\n" + 
-				"    }\n" + 
-				"  }\n" + 
-				"}";
-		
-		
-		//List<EntitiesResult> sample = new ArrayList<>();
-		sample = new ArrayList<>();
-		sample.add(sampleEntityPerson);
-		sample.add(sampleEntityNonPerson);
+		sampleEntityList = new ArrayList<>();
+		sampleEntityList.add(sampleEntityPerson);
+		sampleEntityList.add(sampleEntityNonPerson);
 		
 	}
 
+/// TESTS FOR DOCUMENT EMOTION PARSING /////////////////////////////////////////////////////////		
 	
-	
-	
-	
-	
-	
+	@Test
+	public void testParseDocForAngerScore() {
+		wp.parseDocEmotion(sampleDocumentEmotion);
+		assertEquals(0.144436,wp.getAngerScore(),0.000000);	
+	}
 	
 	@Test
 	public void testParseDocForDisgustScore() {
-		wp.parsePersonEntity(sampleEntityPerson);
-		assertEquals(0.026196,wp.getDisgustScore(),0.000000);	
+		wp.parseDocEmotion(sampleDocumentEmotion);
+		assertEquals(0.074289,wp.getDisgustScore(),0.000000);	
 	}
 
 	
 	@Test
 	public void testParseDocForFearScore() {
-		wp.parsePersonEntity(sampleEntityPerson);
-		assertEquals(0.116198,wp.getFearScore(),0.000000);
+		wp.parseDocEmotion(sampleDocumentEmotion);
+		assertEquals(0.130087,wp.getFearScore(),0.000000);
 	}
 	
 	
 	@Test
 	public void testParseDocForJoyScore() {
-		wp.parsePersonEntity(sampleEntityPerson);
-		assertEquals(0.244804,wp.getJoyScore(),0.000000);
+		wp.parseDocEmotion(sampleDocumentEmotion);
+		assertEquals(0.599415,wp.getJoyScore(),0.000000);
 		
 	}
 	
 	
 	@Test
 	public void testParseDocForSadnessScore() {
-		wp.parsePersonEntity(sampleEntityPerson);
-		assertEquals(0.112424,wp.getSadnessScore(),0.000000);
+		wp.parseDocEmotion(sampleDocumentEmotion);
+		assertEquals(0.519859,wp.getSadnessScore(),0.000000);
+	}
+
+	@Test(expected=JsonParseException.class)
+	public void testWrongFormatting() {
+		String sampleWrongFormat = 
+				"    \"emotion\": {\n" + 
+				"      \"anger\": 0.144436,\n" + 
+				"      \"disgust\": 0.074289,\n" + 
+				"      \"fear\": 0.130087,\n" + 
+				"      \"joy\": 0.599415,\n" + 
+				"    }\n";
+
+		
+		wp.parseDocEmotion(sampleWrongFormat);
+		assertEquals(0.519859,wp.getSadnessScore(),0.000000);
+		
 	}
 	
 	
+/// TESTS FOR ENTITY PARSING ////////////////////////////////////////////////////////////////////
+/// (entity feature not used in GUI version 1.0) 
+
 	
+//	cannot import "assertFalse"	
+/*	
 	@Test
 	public void testParseEntityIsAPerson() {
 		
@@ -124,11 +147,19 @@ public class WatsonParserTest {
 		fail("WatsonParser could not identify a Person entity type");
 	}
 	 
+	@Test(expected=Exception.class)
+	public void testParseEntityIsNotAPerson() {
+		
+		wp.parsePersonEntity(sampleEntityNonPerson);
+		assertFalse("Person",wp.getEntityType());	
+		fail("WatsonParser incorrectly identified a Person entity type");
+	}
+*/	 
 	
 	@Test
-	public void testParseRemoveNonPerson( ) {
-		wp.removeNonPerson(sample);
-		assertEquals(sampleEntityPerson,sample.get(0));
+	public void testParseRemoveNonPerson() {
+		wp.removeNonPerson(sampleEntityList);
+		assertEquals(sampleEntityPerson,sampleEntityList.get(0));
 	}
 	 
 	
