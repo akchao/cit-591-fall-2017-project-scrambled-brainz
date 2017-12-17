5971c312f4505eb704647fe1fecb256d017c3d30
Alice Chao <Alice@Alices-MBP.fios-router.home>
2017-12-17 14:38:54 -0500

    added comments in WatsonParser and exception handling

--- GUI/src/application/WatsonParser.java
+++ GUI/src/application/WatsonParser.java
@@ -6,7 +6,13 @@ import com.google.gson.JsonParser;
 
 
 /**
- * Parses the Watson JSON responses for analysis
+ * Class that contains methods to parse Watson NLU JSON responses 
+ * using a Google GSON API.  
+ * Currently has a method for parsing the emotion response for a 
+ * whole document, as well as methods for cleaning and parsing 
+ * responses for an entity/character's emotions.
+ * Methods are public so they can be called appropriately and 
+ * named to indicate how it should be used.
  * @author Alice
  *
  */
@@ -25,53 +31,93 @@ public class WatsonParser {
 
 	
 	/**
-	 * Constructs Watson response parser
+	 * Constructs a Watson JSON response parser object
 	 */
 	public WatsonParser() {
-//		WatsonAnalyzer wa = new WatsonAnalyzer();
-//		AnalysisResults watsonResponse = wa.getWatsonResponse();
-//		List<EntitiesResult> watsonEntities = wa.getWatsonEntities();
-//		removeNonPerson(watsonEntities);
+
 	}
 	
 	
-/*	
-{
-  "document": {
-    "emotion": {
-      "anger": 0.144436,
-      "disgust": 0.074289,
-      "fear": 0.130087,
-      "joy": 0.599415,
-      "sadness": 0.519859
-    }
-  }
-}
-*/
-
+	/**
+	 * Method to parse a document's emotion, Json response must be formatted as a string
+	 * and only takes in one document emotion at a time, not entire Watson response.
+	 * @param text the Watson Json response for document emotion
+	 */
 	public void parseDocEmotion(String text) {
-		
-		JsonObject emotionResponse = new JsonParser().parse(text)
-				.getAsJsonObject();
-		
-		JsonObject docResponse = emotionResponse.get("document").getAsJsonObject();
-		
-		parseEmotion(docResponse);		
+		try {
+			JsonObject emotionResponse = new JsonParser().parse(text)
+					.getAsJsonObject();
+
+			JsonObject docResponse = emotionResponse.get("document").getAsJsonObject();
+
+			parseEmotion(docResponse);	
+		} catch (Exception e) {
+			System.out.println("Provided document emotion was formatted incorrectly ");
+		}
 		
 	}
 	
+	/**
+	 * Method that parses just the emotion Json object for 5 emotions. 
+	 * @param response a JsonObject of just the Watson emotion data
+	 */
 	public void parseEmotion(JsonObject response) {
-
-		JsonObject emotion = response.get("emotion").getAsJsonObject();
-		angerScore = emotion.get("anger").getAsDouble();
-		disgustScore = emotion.get("disgust").getAsDouble();
-		fearScore = emotion.get("fear").getAsDouble();
-		joyScore = emotion.get("joy").getAsDouble();
-		sadnessScore = emotion.get("sadness").getAsDouble();
+		try {
+			JsonObject emotion = response.get("emotion").getAsJsonObject();
+			angerScore = emotion.get("anger").getAsDouble();
+			disgustScore = emotion.get("disgust").getAsDouble();
+			fearScore = emotion.get("fear").getAsDouble();
+			joyScore = emotion.get("joy").getAsDouble();
+			sadnessScore = emotion.get("sadness").getAsDouble();
+		} catch (Exception e) {
+			System.out.println("Provided emotion was formatted incorrectly ");
+		}
 
 	}
 	
 	
+	/**
+	 * @return the angerScore Watson's anger score for the document or an entity
+	 */
+	public double getAngerScore() {
+		return angerScore;
+	}
+
+	/**
+	 * @return the disgustScore Watson's disgust score for the document or an entity 
+	 */
+	public double getDisgustScore() {
+		return disgustScore;
+	}
+
+	/**
+	 * @return the fearScore Watson's fear score for the document or an entity
+	 */
+	public double getFearScore() {
+		return fearScore;
+	}
+
+	/**
+	 * @return the joyScore Watson's joy score for the document or an entity
+	 */
+	public double getJoyScore() {
+		return joyScore;
+	}
+
+	/**
+	 * @return the sadnessScore Watson's sadness score for the document or an entity
+	 */
+	public double getSadnessScore() {
+		return sadnessScore;
+	}
+	
+	
+//// ADDITIONAL FUNCTIONALITIES NOT USED IN 1.0 VERSION OF THE GUI /////////////////////////////////
+	
+	/**
+	 * Removes all entities from an arraylist that are not of "Person" type 
+	 * @param watsonResponse
+	 */
 	public void removeNonPerson(List<String> watsonResponse) {
 		
 		for (int i=0; i<watsonResponse.size(); i++) {
@@ -91,8 +137,8 @@ public class WatsonParser {
 	} 
 	
 	/**
-	 * Parses a single Person entity response
-	 * @param text
+	 * Parses a single Person entity response for emotion scores
+	 * @param text a single Person entity response as a string
 	 */
 	public void parsePersonEntity(String text) {
 		JsonObject entityResponse = new JsonParser().parse(text)
@@ -115,70 +161,35 @@ public class WatsonParser {
 	
 
 	/**
-	 * @return the type only for 
+	 * @return the type the Watson entity type (e.g. "Person" or "Location")
 	 */
 	public String getEntityType() {
 		return entityType;
 	}
 	
 	/**
-	 * @return the name
+	 * @return the name the name of the entity (e.g. character)
 	 */
 	public String getEntityName() {
 		return entityName;
 	}
 	
 	/**
-	 * @return the relevance
+	 * @return the relevance the entity's importance in the book 
 	 */
 	public double getEntityRelevance() {
 		return entityRelevance;
 	}
 
 	/**
-	 * @return the count
+	 * @return the count the number of times the entity appears in the book
 	 */
 	public int getEntityCount() {
 		return entityCount;
 	}
 
 	/**
-	 * @return the angerScore
-	 */
-	public double getAngerScore() {
-		return angerScore;
-	}
-
-	/**
-	 * @return the disgustScore
-	 */
-	public double getDisgustScore() {
-		return disgustScore;
-	}
-
-	/**
-	 * @return the fearScore
-	 */
-	public double getFearScore() {
-		return fearScore;
-	}
-
-	/**
-	 * @return the joyScore
-	 */
-	public double getJoyScore() {
-		return joyScore;
-	}
-
-	/**
-	 * @return the sadnessScore
-	 */
-	public double getSadnessScore() {
-		return sadnessScore;
-	}
-
-	/**
-	 * @return the sentimentScore
+	 * @return the entitySentimentScore Watson's sentiment score for only an entity
 	 */
 	public double getEntitySentimentScore() {
 		return entitySentimentScore;
