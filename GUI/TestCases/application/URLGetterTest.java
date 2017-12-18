a6f0e5061b7ce79998f608c081e0dd4559e128bd
drawjk705 <drawjk705@gmail.com>
2017-12-18 09:40:30 -0500

    deleted Test folder

--- GUI/TestCases/application/URLGetterTest.java
+++ GUI/TestCases/application/URLGetterTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import preliminary_work.URLGetter;
+
 public class URLGetterTest {
 
+	/**
+	 * make sure that links are all for .txt
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLinksAreTxt() {
+		URLGetter urlg = new URLGetter();
 	}
 
 }
